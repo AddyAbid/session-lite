1e33fc6eb7ebc19b979c52789feb2651b0947cf3
Addy Abid <addy.abeed@gmail.com>
2021-12-23 01:40:53 +0000

    copied env

--- database/data.sql
+++ database/data.sql
@@ -1,5 +1,5 @@
 insert into "users" ("userId", "username", "email", "password")
-values ('1', 'admin', 'admin@gmail.com', 'admin1')
+values (1, 'admin', 'admin@gmail.com', 'admin1')
 
 insert into "posts" ("userId", "postId", "title", "description", "imgUrl")
-values ('1', '1', 'test1', 'this is a test', '/images/image-1608316435111.png')
+values (1, 1, 'test1', 'this is a test', '/images/image-1608316435111.png')
