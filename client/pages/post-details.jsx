721ad0167dae23526fb9a6e6d49233b76706925f
Addy Abid <addy.abeed@gmail.com>
2021-12-28 19:07:49 +0000

    Update server/index.js to handle get request

---
+++ client/pages/post-details.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 1
+    };
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default Details;
