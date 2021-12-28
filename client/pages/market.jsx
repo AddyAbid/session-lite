43b611db3f46b76577d16861de09d47b443ac88c
Addy Abid <addy.abeed@gmail.com>
2021-12-28 18:39:05 +0000

    removed plural parameter name in map to represent a single post

--- client/pages/market.jsx
+++ client/pages/market.jsx
@@ -21,15 +21,15 @@ class MarketPlace extends React.Component {
       <div className='container'>
         <div className='market-row'>
           {this.state.posts.map(
-            posts => {
+            post => {
               return (
-          <div className='column-half pd-7 mb-desktop mr-2rem-desktop' key={posts.postId}>
-            <img src={posts.imgUrl} className=' width-100 border-5' />
-            <h4 className='margin-0 roboto-5 hide-mobile'>{posts.title}</h4>
-            <h4 className='margin-0 raleway-400-price pt-8 hide-mobile'>${posts.price}</h4>
+          <div className='column-half pd-7 mb-desktop mr-2rem-desktop' key={post.postId}>
+            <img src={post.imgUrl} className=' width-100 border-5' />
+            <h4 className='margin-0 roboto-5 hide-mobile'>{post.title}</h4>
+            <h4 className='margin-0 raleway-400-price pt-8 hide-mobile'>${post.price}</h4>
             <div className='column-100 relative hide-desktop'>
               <div className='absolute-background'>
-                <h3 className='margin-0 text-align-center raleway-500-price'>${posts.price}</h3>
+                <h3 className='margin-0 text-align-center raleway-500-price'>${post.price}</h3>
               </div>
             </div>
           </div>
