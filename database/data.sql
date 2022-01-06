4585a4a8516e17aa24c404af66871a1a95812a8d
Addy Abid <addy.abeed@gmail.com>
2022-01-06 23:12:24 +0000

    Insert username, hashedPassword, and email into users table

--- database/data.sql
+++
@@ -1,3 +0,0 @@
-insert into "users" ("userId", "username", "email", "password")
-values (1, 'admin', 'admin@gmail.com', '$argon2i$v=19$m=4096,t=3,p=1$xLuz/3vnLsdimbGkDkKSlA$dU0eaNiJzQtiqk92DdLwjc83rWusEvNgY6E+0QHSybs'),
-(2, 'admin2', 'admin2@gmail.com', '$argon2i$v=19$m=4096,t=3,p=1$bnFReGHYkvo4LmTa0BoxgA$f7aO7w6gAKNSUf3lW20DjrwGhdwDQ9alP6ZHpasZ/aE');
