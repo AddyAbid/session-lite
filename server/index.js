abba3c4b6e01e78846172385e59ad21d2e266004
Addy Abid <addy.abeed@gmail.com>
2022-01-13 22:49:14 +0000

    Changed token secret

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ io.on('connection', socket => {
   const seller = Number(socket.handshake.query.seller);
   const postId = socket.handshake.query.postId;
   jwt
-    .verify(token, 'changeMe', (err, verifiedToken) => {
+    .verify(token, '*WTJGLqcq2(5w*{M', (err, verifiedToken) => {
       if (err) {
         verifiedToken = null;
       } else {
