a9a65a0287cd2ae60983da8b327d2f9af535e980
Addy Abid <addy.abeed@gmail.com>
2022-03-05 03:50:52 +0000

    marketplace layout refactor - in progress

--- client/pages/market.jsx
+++ client/pages/market.jsx
@@ -23,11 +23,11 @@ class MarketPlace extends React.Component {
           this.state.posts.length === 0 &&
           <h2 className='font-20px margin-top-15 roboto-medium modal-row justify-content-center'>Sorry, there are currently no sessions to browse ):</h2>
         }
-        <div className='market-row'>
+        <div className='marketplace-row justify-content-center'>
           {this.state.posts.map(
             post => {
               return (
-          <div className='column-half pd-7 mb-desktop mr-2rem-desktop' key={post.postId} >
+          <div className=' pd-7 mb-desktop mr-2rem-desktop' key={post.postId} >
           <a href={`#posts?postId=${post.postId}`}>  <img src={post.imgUrl} className=' object-fit-market border-5' /></a>
             <h4 className='margin-0 roboto-5 hide-mobile'>{post.title}</h4>
             <h4 className='margin-0 raleway-400-price pt-8 hide-mobile'>${post.price}</h4>
