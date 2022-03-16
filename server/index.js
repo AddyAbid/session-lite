b10e435132e1d7c9a91f5d31ac7ccc83cc566d16
Addy Abid <addy.abeed@gmail.com>
2022-03-16 01:57:08 +0000

    imported token secret through env module to prevent vulnerabilities

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ io.on('connection', socket => {
   const seller = Number(socket.handshake.query.seller);
   const postId = socket.handshake.query.postId;
   jwt
-    .verify(token, 'D8K?Aa$+pdfu_Z5T', (err, verifiedToken) => {
+    .verify(token, process.env.TOKEN_SECRET, (err, verifiedToken) => {
       if (err) {
         verifiedToken = null;
       } else {
